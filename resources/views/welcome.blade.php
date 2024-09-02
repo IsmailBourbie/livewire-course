9ff3913b2ec9e4b954d1714dae2d53f63033a6fb
Ismail Bourbie <bourbieismail@gmail.com>
2024-09-02 13:51:45 +0100

    Changed colors on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
 <body>
 <div class="min-h-screen flex justify-center items-center uppercase bg-orange-100 text-center">
     <div>
-        <h1 class="text-6xl font-bold tracking-wider text-red-400 mb-16">Welcome to livewire Course</h1>
-        <a href="/counter" class="py-3 px-6 border border-blue-700 bg-blue-600 text-orange-100 rounded">Get Started</a>
+        <h1 class="text-7xl font-bold tracking-wider text-transparent mb-16 bg-gradient-to-r from-teal-700 to-red-400 bg-clip-text">livewire Course</h1>
+        <a href="/counter" class="py-3 px-6 border border-blue-400 bg-gradient-to-br from-sky-700 to-sky-800 text-sky-100 rounded">Get Started</a>
     </div>
 </div>
 </body>
