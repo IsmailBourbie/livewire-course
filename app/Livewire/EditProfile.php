9e273d57bd7633d0649ac11388db7a50d9d610f0
Ismail Bourbie <bourbieismail@gmail.com>
2024-09-09 13:48:04 +0100

    Added basic update-profile function

--- app/Livewire/EditProfile.php
+++ app/Livewire/EditProfile.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Illuminate\View\View;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -9,6 +10,25 @@ use Livewire\Component;
 #[Title('Edit Profile')]
 class EditProfile extends Component
 {
+    public User $user;
+    public string $username;
+    public ?string $bio;
+
+    public function update(): void
+    {
+        $this->user->username = $this->username;
+        $this->user->bio = $this->bio;
+        $this->user->save();
+    }
+
+    public function mount(): void
+    {
+        $this->user = auth()->user();
+        $this->username = $this->user->username;
+        $this->bio = $this->user->bio;
+
+    }
+
     public function render(): View
     {
         return view('livewire.edit-profile');
--- resources/views/livewire/edit-profile.blade.php
+++ resources/views/livewire/edit-profile.blade.php
@@ -1,12 +1,14 @@
 <div class="bg-blue-100 rounded min-h-screen flex items-center justify-center">
     <div class="p-16 bg-white rounded-lg shadow-xl w-5/12">
         <h1 class="text-2xl mb-8 text-center font-bold">Update Your Profile..</h1>
-        <form action="" method="post" class="space-y-6">
+        <form action="" method="post" class="space-y-6" wire:submit.prevent="update">
             <div>
                 <label for="username" class="block text-sm font-medium mb-2">Username</label>
                 <input type="text" id="username" name="title"
                        class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500"
-                       placeholder="Enter Username">
+                       placeholder="Enter Username"
+                       wire:model="username"
+                >
                 @error('username')
                 <em class="py-1 px-2 text-sm text-red-400 font-bold">{{ $message }}</em>
                 @enderror
@@ -18,6 +20,7 @@
                           name="Bio"
                           maxlength="100"
                           rows="4" placeholder="A little bit about yourself..."
+                          wire:model="bio"
                 ></textarea>
                 @error('bio')
                 <em class="py-1 px-2 text-sm text-red-400 font-bold">{{ $message }}</em>
@@ -26,8 +29,8 @@
             </div>
             <div class="w-full flex space-x-2">
                 <button type="submit"
-                        class="w-full py-2 px-6 text-center text-sm font-medium rounded-lg border border-transparent bg-teal-400 text-teal-800 hover:bg-teal-300 focus:outline-none focus:bg-teal-200 disabled:opacity-50 disabled:pointer-events-none">
-                    Save
+                        class="w-full py-2 px-6 text-center text-sm font-medium rounded-lg border border-transparent bg-blue-400 text-blue-800 hover:bg-blue-300 focus:outline-none focus:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none">
+                    Update
                 </button>
                 <button type="reset"
                         class="w-full py-2 px-6 text-center text-sm font-medium rounded-lg border border-transparent bg-red-300 text-red-800 hover:bg-red-200 focus:outline-none focus:bg-red-200 disabled:opacity-50 disabled:pointer-events-none">
