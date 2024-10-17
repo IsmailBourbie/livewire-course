4b817b139f4f7967838794d447e45eb32f3609e0
Ismail Bourbie <bourbieismail@gmail.com>
2024-10-17 14:42:32 +0100

    add url to selectedProductsIds filter

--- app/Livewire/Order/Index/Forms/Filters.php
+++ app/Livewire/Order/Index/Forms/Filters.php
@@ -4,17 +4,19 @@ namespace App\Livewire\Order\Index\Forms;
 
 use App\Models\Store;
 use Illuminate\Database\Eloquent\Builder;
+use Livewire\Attributes\Url;
 use Livewire\Form;
 
 class Filters extends Form
 {
     public Store $store;
+    #[Url]
     public array $selectedProductsIds = [];
 
     public function init(Store $store): void
     {
         $this->store = $store;
-        $this->selectedProductsIds = $this->store->products->pluck('id')->toArray();
+        $this->initSelectedProductIds();
     }
 
     public function products()
@@ -25,6 +27,13 @@ class Filters extends Form
     public function apply(Builder $query): Builder
     {
         return $query->whereIn('product_id', $this->selectedProductsIds);
+    }
+
+    public function initSelectedProductIds(): void
+    {
+        if (empty($this->selectedProductsIds)) {
+            $this->selectedProductsIds = $this->products()->pluck('id')->toArray();
+        }
     }
 
 }
